53eed5fa0070331ca0de7656669e9db7de546cbd
muhammad arifin alfianto <fianarifin6@gmail.com>
2024-06-10 02:43:52 +0700

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,103 @@
 @extends('layouts.auth')
 
 @section('content')
-<div id="wrapper">
+    <div id="wrapper">
 
- <div class="loader-wrapper"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>
-	<div class="card card-authentication1 mx-auto my-5">
-		<div class="card-body">
-		 <div class="card-content p-2">
-		 	<div class="text-center">
-		 		<img src="assets/images/apiibaru.png" alt="logo icon">
-		 	</div>
-		  <div class="card-title text-uppercase text-center py-3">Sign In</div>
-		    <form class="mt-4" action="{{ route('login') }}" method="POST">
-                @csrf
+        <div class="loader-wrapper">
+            <div class="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <div class="card card-authentication1 mx-auto my-5">
+            <div class="card-body">
+                <div class="card-content p-2">
+                    <div class="text-center">
+                        <img src="assets/images/apiibaru.png" alt="logo icon">
+                    </div>
+                    <div class="card-title text-uppercase text-center py-3">Sign In</div>
+                    <form class="mt-4" action="{{ route('login') }}" method="POST">
+                        @csrf
 
-			  <div class="form-group">
-			  <label for="exampleInputEmail">Email</label>
-			   <div class="position-relative has-icon-right">
-				  <input name="email" type="email" id="exampleInputEmail" class="form-control input-shadow" placeholder="Enter Email">
-				  <div class="form-control-position">
-					  <i class="icon-user"></i>
-				  </div>
-			   </div>
-			  </div>
-			  <div class="form-group">
-			  <label for="exampleInputPassword" class="sr-only">Password</label>
-			   <div class="position-relative has-icon-right">
-				  <input name="password" type="password" id="exampleInputPassword" class="form-control input-shadow" placeholder="Enter Password">
-				  <div class="form-control-position">
-					  <i class="icon-lock"></i>
-				  </div>
-			   </div>
-			  </div>
-			<div class="form-row">
-			 <div class="form-group col-6">
-			   <div class="icheck-material-white">
-                <input name="remember" type="checkbox" id="user-checkbox" checked="" />
-                <label for="user-checkbox">Remember me</label>
-			  </div>
-			 </div>
-			</div>
-			 <button type="submit" class="btn btn-light btn-block">Sign In</button>
-			</div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail">Email</label>
+                            <div class="position-relative has-icon-right">
+                                <input name="email" type="email" id="exampleInputEmail"
+                                    class="form-control input-shadow" placeholder="Enter Email">
+                                <div class="form-control-position">
+                                    <i class="icon-user"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword" class="sr-only">Password</label>
+                            <div class="position-relative has-icon-right">
+                                <input name="password" type="password" id="exampleInputPassword"
+                                    class="form-control input-shadow" placeholder="Enter Password">
+                                <div class="form-control-position">
+                                    <i class="icon-lock"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-6">
+                                <div class="icheck-material-white">
+                                    <input name="remember" type="checkbox" id="user-checkbox" checked="" />
+                                    <label for="user-checkbox">Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-light btn-block">Sign In</button>
+                </div>
 
-			 </form>
-		   </div>
-		  </div>
-		  <div class="card-footer text-center py-3">
-		    <p class="text-warning mb-0">Do not have an account? <a href="{{ route('register') }}"> Sign Up here</a></p>
-		  </div>
-	     </div>
+                </form>
+            </div>
+        </div>
+        <div class="card-footer text-center py-3">
+            <p class="text-warning mb-0">Do not have an account? <a href="{{ route('register') }}"> Sign Up here</a></p>
+        </div>
+    </div>
 
-     <!--Start Back To Top Button-->
+    <!--Start Back To Top Button-->
     <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
     <!--End Back To Top Button-->
 
-	<!--start color switcher-->
-   <div class="right-sidebar">
-    <div class="switcher-icon">
-      <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
+    <!--start color switcher-->
+    <div class="right-sidebar">
+        <div class="switcher-icon">
+            <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
+        </div>
+        <div class="right-sidebar-content">
+
+            <p class="mb-0">Gaussion Texture</p>
+            <hr>
+
+            <ul class="switcher">
+                <li id="theme1"></li>
+                <li id="theme2"></li>
+                <li id="theme3"></li>
+                <li id="theme4"></li>
+                <li id="theme5"></li>
+                <li id="theme6"></li>
+            </ul>
+
+            <p class="mb-0">Gradient Background</p>
+            <hr>
+
+            <ul class="switcher">
+                <li id="theme7"></li>
+                <li id="theme8"></li>
+                <li id="theme9"></li>
+                <li id="theme10"></li>
+                <li id="theme11"></li>
+                <li id="theme12"></li>
+                <li id="theme13"></li>
+                <li id="theme14"></li>
+                <li id="theme15"></li>
+            </ul>
+
+        </div>
     </div>
-    <div class="right-sidebar-content">
-
-      <p class="mb-0">Gaussion Texture</p>
-      <hr>
-
-      <ul class="switcher">
-        <li id="theme1"></li>
-        <li id="theme2"></li>
-        <li id="theme3"></li>
-        <li id="theme4"></li>
-        <li id="theme5"></li>
-        <li id="theme6"></li>
-      </ul>
-
-      <p class="mb-0">Gradient Background</p>
-      <hr>
-
-      <ul class="switcher">
-        <li id="theme7"></li>
-        <li id="theme8"></li>
-        <li id="theme9"></li>
-        <li id="theme10"></li>
-        <li id="theme11"></li>
-        <li id="theme12"></li>
-		<li id="theme13"></li>
-        <li id="theme14"></li>
-        <li id="theme15"></li>
-      </ul>
-
-     </div>
-   </div>
 @endsection
